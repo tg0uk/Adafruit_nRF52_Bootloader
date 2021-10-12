5ea4c35b7b05693518b571bd9673e60b85d70080
glucee <glucee@hotmail.com>
2021-10-12 09:36:53 +0100

    disable the DFU entering when start

--- src/main.c
+++ src/main.c
@@ -233,7 +233,7 @@ int main(void)
       bootloader_dfu_start(false, DFU_SERIAL_STARTUP_INTERVAL, false);
 #else
       // if RST is pressed during this delay --> if will enter dfu
-      NRFX_DELAY_MS(DFU_DBL_RESET_DELAY);
+      // NRFX_DELAY_MS(DFU_DBL_RESET_DELAY);
 #endif
     }
 
